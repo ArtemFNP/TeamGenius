9ee20d1f22bead217db46112949fba037fa3a9d1
NureFilatovArtem <artem.filatov@nure.ua>
2025-05-30 01:38:35 +0200

    New Navbar, design preparation

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from '../components/Navbar'; // Убедись, что путь верный
+import WeatherDashboard from './WeatherDashboard'; // Твоя главная страница
+import Wardrobe from './Closet'; // Твоя страница 
 import WeatherDashboard from './WeatherDashboard';
 import OutfitSelector from './OutfitSelector';
-import Closet from './Closet';
+
 import OutfitPage from './OutfitPage';
 import '../styles/App.css';
+import '../styles/variables.css';
+import '../styles/Navbar.css';
 import TimelineSelector from './TimelineSelector';
 
 function App() {
